38d16fb18e192102dee6d36c00f73543851a5dff
Marius Nettum <nettum@gmail.com>
2020-03-30 20:47:30 +0200

    Add upcoming label for upcoming shows

--- web/pages/index.js
+++ web/pages/index.js
@@ -5,24 +5,34 @@ import client from '../client'
 import Layout from '../components/layout';
 import styles from './index.module.css';
 
+
+const renderItem = (item)  => {
+  const { _id, title, slug, poster, streamDate } = item;
+
+  const airDate = new Date(streamDate);
+  const now = new Date();
+
+  return (
+    <div key={_id} className={styles.item}>
+      <Link href="/stream/[slug]" as={`/stream/${slug.current}`}>
+        <a>
+          {poster && <img src={`${poster}?w=350&h=197&crop=center&fit=crop`} alt={`Skjermbilde av ${title}`} />}
+          <h2>{title}</h2>
+          <small>{new Intl.DateTimeFormat('nb-NO', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'} ).format(new Date(streamDate))}</small>
+          {now < airDate && <small className={styles.upcoming}>Kommende</small>}
+        </a>
+      </Link>
+    </div>
+  )
+};
+
 const Index = (props) => {
   const { streams = [] } = props;
 
   return (
     <Layout>
       <section className={styles.streams}>
-      {streams.map(
-        ({ _id, title, slug, poster, streamDate }) =>
-        <div key={_id} className={styles.item}>
-          <Link href="/stream/[slug]" as={`/stream/${slug.current}`}>
-            <a>
-              {poster && <img src={`${poster}?w=350&h=197&crop=center&fit=crop`} alt={`Skjermbilde av ${title}`} />}
-              <h2>{title}</h2>
-              <small>{new Intl.DateTimeFormat('nb-NO', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'} ).format(new Date(streamDate))}</small>
-            </a>
-          </Link>
-        </div>
-      )}
+        {streams.map(item => (renderItem(item)))}
       </section>
     </Layout>
   )
