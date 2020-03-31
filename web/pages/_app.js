1fb4cd070d752f357de7fc7e5decba9945810498
Marius Nettum <nettum@gmail.com>
2020-03-31 17:05:51 +0200

    Add SEO/share data
    Closes #1

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
+
 import '../styles.css'
 
 import Layout from '../components/layout';
@@ -6,6 +8,16 @@ import Layout from '../components/layout';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextSeo
+        title="Karantenekonserter.no"
+        description="Karantenekonserter.no er en nettside som samler alle norske konsertstrømmer på en og samme plass."
+        openGraph={{
+          type: 'website',
+          url: 'https://karantenekonserter.no',
+          title: 'Karantenekonserter.no',
+          description: 'STØTT NORSKE ARTISTER! Se opptak og hvilke konserter som kommer for strømming på karantenekonserter.no',
+        }}
+      />
       <Head>
         <link
           href="https://fonts.googleapis.com/css?family=Bitter|Open+Sans&display=swap"
--- web/pages/stream/[slug].js
+++ web/pages/stream/[slug].js
@@ -1,3 +1,5 @@
+import { NextSeo } from 'next-seo';
+
 import client from '../../client';
 import BlockContent from '@sanity/block-content-to-react';
 
@@ -22,7 +24,17 @@ const renderStream = (props) => {
 };
 
 const Stream = (props) => {
-  const { title, streamDate, description } = props;
+  const { title, slug, poster, streamDate, description } = props;
+
+  const readableDate = new Intl.DateTimeFormat('nb-NO', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'}
+    ).format(new Date(streamDate));
+
+  // const shareImage = {`${poster}?w=1200&h=630&crop=center&fit=crop`};
 
   const serializers = {
     marks: {
@@ -35,11 +47,28 @@ const Stream = (props) => {
 
   return (
     <article className={styles.main}>
+      <NextSeo
+        title={`${title} | karantenekonserter.no`}
+        description={`Se ${title} og flere konserter på karantenekonserter.no`}
+        openGraph={{
+          type: 'website',
+          url: `https://karantenekonserter.no/stream/${slug.current}`,
+          title: `${title} | karantenekonserter.no`,
+          description: 'STØTT NORSKE ARTISTER! Se opptak og hvilke konserter som kommer for strømming på karantenekonserter.no',
+          images: [
+            {
+              url: `${poster}?w=1200&h=630&crop=center&fit=crop`,
+              width: 1200,
+              height: 630,
+            },
+          ],
+        }}
+      />
       {renderStream(props)}
       <div className={styles.intro}>
         <div>
           <h1>{title}</h1>
-          <small>{new Intl.DateTimeFormat('nb-NO', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'} ).format(new Date(streamDate))}</small>
+          <small>{readableDate}</small>
         </div>
         <ul>
           {/* <li>1</li>
