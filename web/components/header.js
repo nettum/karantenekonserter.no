53ebea38711b87d6a3597371a22ad89b3a8f9fe2
Marius Nettum <nettum@gmail.com>
2020-03-31 19:13:47 +0200

    Add header

--- web/components/header.js
+++ web/components/header.js
@@ -4,8 +4,14 @@ import styles from './header.module.css';
 
 const Header = () => (
   <header className={styles.main}>
-    <Link href="/">
-      <a><h1>Karantenekonserter.no</h1></a>
+    <Link className={styles.logo} href="/">
+      <a>
+        <div className={styles.emoji}>
+          <img src="heart.png" alt="Hjerte emoji ❤️" />
+          <img src="horns.png" alt="Djevelhorn emoji 🤘" />
+        </div>
+        <h1>Karantenekonserter.no</h1>
+      </a>
     </Link>
     <Link href="/about">
       <a className={styles.about}>Om</a>
