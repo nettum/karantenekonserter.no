9cb1c16651a5b8cd7056c96202de53e480d00dc5
Marius Nettum <nettum@gmail.com>
2020-04-04 17:34:10 +0200

    Scroll input field to top when focus on search

--- web/components/streams.js
+++ web/components/streams.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import Stream from './stream';
 import styles from './streams.module.css';
@@ -6,9 +6,14 @@ import styles from './streams.module.css';
 const Streams = props => {
   const { status } = props;
   const [query, setSearchQuery] = useState('');
+  const searchInput = useRef(null);
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
+  }
+
+  const handleFocus = (e) => {
+    searchInput.current.scrollIntoView({behavior: 'smooth'});
   }
 
   const streams = !query ?
@@ -22,7 +27,7 @@ const Streams = props => {
     <section>
       <div className={styles.sectionheader}>
         <h2>{status} <small>({streams.length})</small></h2>
-        {status === 'Arkiv' && <input type="text" placeholder="Søk" value={query} onChange={handleSearch} />}
+        {status === 'Arkiv' && <input ref={searchInput} type="text" placeholder="Søk" value={query} onChange={handleSearch} onFocus={handleFocus} />}
       </div>
       <div className={styles.streams}>
         {streams.map(stream => <Stream key={stream._id} stream={stream} status={status} />)}
