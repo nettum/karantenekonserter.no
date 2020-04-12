9950fd54a6d6c75661ae6a2d643c7533477d8095
Marius Nettum <nettum@gmail.com>
2020-04-12 16:18:00 +0200

    Support more youtube URLs than /embed/<code>
    Closes #19

--- web/components/youtubevideo.js
+++ web/components/youtubevideo.js
@@ -1,17 +1,24 @@
+import getYouTubeID from 'get-youtube-id';
+
 import styles from './youtubevideo.module.css'
 
-const YoutubeVideo = props => (
-  <div className={styles.container}>
-    <iframe
-      src={props.url}
-      width="1024"
-      height="576"
-      className={styles.iframe}
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen>
-    </iframe>
-  </div>
-);
+const YoutubeVideo = props => {
+
+  const youtubeId = getYouTubeID(props.url);
+
+  return (youtubeId ?
+    <div className={styles.container}>
+      <iframe
+        src={`https://www.youtube-nocookie.com/embed/${youtubeId}`}
+        width="1024"
+        height="576"
+        className={styles.iframe}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen>
+      </iframe>
+    </div>
+  : null)
+};
 
 export default YoutubeVideo;
